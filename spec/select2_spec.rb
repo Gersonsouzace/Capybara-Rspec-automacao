3732d80d433f90e5e822ccda7b76f956e6a95ff6
Gersonsouzace <gersonsouzace@gmail.com>
2020-05-04 12:00:41 -0300

    exemplo select2 simple e mult

--- spec/select2_spec.rb
+++ spec/select2_spec.rb
@@ -20,9 +20,27 @@ describe 'Select 2', :select2 do
     end
 
 
-    describe('mutiple') do
+    describe('mutiple', :multi) do
         before(:each)do
-            visit '/apps/select2/multi.html'
+            visit 'https://training-wheels-protocol.herokuapp.com/apps/select2/multi.html'
+        end
+
+        def seleciona(ator)
+            find('.select2-selection--multiple').click
+            find('.select2-search__field').set ator
+            find('.select2-results__option').click
+        end
+
+        it 'seleciona atores' do
+
+            atores = ['Jim Carrey', 'Owen Wilson', 'Kevin James']
+
+            atores.each do |a|
+                seleciona(a)
+            end
+
+            #seleciona('Jim Carrey')
+            #seleciona('Owen Wilson')
         end
     end
 
